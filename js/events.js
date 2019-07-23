44d045d3e9da8b2ea72fa02ffd9b374390eafa37
Clark Chen <yyunqingchenn@gmail.com>
2019-07-23 03:34:21 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -13,7 +13,7 @@ function frameIt(){
 }
 
 function pressIt() {
-  $(input).on('keydown', function(key){
+  $('input').on('keydown', function(key){
     if(key.which == 71){
       alert('G was pressed');
     }
