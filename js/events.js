20ba9ce5c3dcb5c9afe7979f734b227733e11ffe
Clark Chen <yyunqingchenn@gmail.com>
2019-07-23 03:32:19 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -26,6 +26,5 @@ function submitIt() {
   $("form").on("submit", function() {
     if ($( "input:first").val() === "correct") {
       alert('Your form is going to be submitted now.')
-      return;
   }})
 }
